a567dfaf9ea06567f43e22a4bb28ca2497ad3e41
Md Sajidur Rahman <sajidul.jami@gmail.com>
2026-07-06 14:57:14 +0600

    failed and try again

--- Images-container-final/scripts/minio-init.sh
+++ Images-container-final/scripts/minio-init.sh
@@ -3,14 +3,15 @@ set -eu
 
 MINIO_HOST="${MINIO_HOST:-minio}"
 MINIO_PORT="${MINIO_PORT:-9000}"
-MINIO_ROOT_USER="${MINIO_ROOT_USER:?MINIO_ROOT_USER is required}"
-MINIO_ROOT_PASSWORD="${MINIO_ROOT_PASSWORD:?MINIO_ROOT_PASSWORD is required}"
+MINIO_ACCESS_KEY="${MINIO_ACCESS_KEY:?MINIO_ACCESS_KEY is required}"
+MINIO_SECRET_KEY="${MINIO_SECRET_KEY:?MINIO_SECRET_KEY is required}"
 MINIO_BUCKET="${MINIO_BUCKET:-ecommerce}"
 
 echo "Waiting for MinIO at ${MINIO_HOST}:${MINIO_PORT}..."
-until mc alias set local "http://${MINIO_HOST}:${MINIO_PORT}" "${MINIO_ROOT_USER}" "${MINIO_ROOT_PASSWORD}"; do
+until mc alias set local "http://${MINIO_HOST}:${MINIO_PORT}" "${MINIO_ACCESS_KEY}" "${MINIO_SECRET_KEY}"; do
   sleep 2
 done
+
 
 mc mb --ignore-existing "local/${MINIO_BUCKET}"
 mc anonymous set download "local/${MINIO_BUCKET}"
